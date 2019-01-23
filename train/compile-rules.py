24a9e4b8fc8240051e283cd2d5ebb1d0d448f875
pkauppin <pekka.kauppinen@helsinki.fi>
2019-01-23 13:26:33 +0200

    Add rule compilation and batch training scripts

---
+++ train/compile-rules.py
@@ -0,0 +1,150 @@
+#! /usr/bin/env python3
+#
+# - Compile parallel replace rules into a FST
+# - Rules have been formulated in a way that allows them
+#   to be compiled separately and then composed.
+#   This is less time-consuming than compiling a single
+#   monolithic rule set.
+# - HFST is required for compilation and composition.
+
+import hfst
+from sys import argv, stderr
+
+eps = '@_EPSILON_SYMBOL_@'
+pad = '"<P>"'
+lbreak = '<LBREAK>'
+
+esc_dict = {
+        '"'    : '%"',
+        eps    : '"<E>"',
+        pad    : pad,
+        lbreak : '"'+lbreak+'"',
+        '\\'   : '"\\\\"',
+        }
+
+
+# Escape special characters
+def esc(chars):
+    S = [ ]
+    for char in chars.split(' '):
+        if char in esc_dict:
+            S.append(esc_dict[char])
+        else:
+            S.append('{'+char+'}')
+    return ' '.join(S)
+
+
+# Retrieve set of known characters from set of features
+def get_chars(feats_file):
+    file = open(feats_file, 'r')
+    feats = [ eval(line.strip()) for line in file ]
+    file.close()
+    chars = { esc(pad) }
+    for (sub, cL, cR) in feats:
+        char = esc(sub[0])
+        chars = chars | { char }
+    return chars
+
+
+def expand(fst):
+    #regex = '"<S>" -> [ "<S>" "<E>" "<.>" "<E>" "<S>" ]'
+    regex = '"<S>" -> [ "<S>" "<E>" "<E>" "<S>" ]'
+    fst.compose(hfst.regex(regex))
+
+
+def serial_compile(regexs):
+
+    #Compile each rule individually
+    queue = [ ]
+    for regex in regexs:
+        fst = hfst.regex(regex)
+        n = fst.number_of_states()
+        queue.append(fst)
+        
+    # Sort resulting FST by number of states
+    queue.sort(key=lambda fst: fst.number_of_states())
+
+    # Compose smallest two, move resulting FST to end of queue
+    n = len(queue)
+    for i in range(n-1):
+        fst1, fst2 = queue[0:2]
+        fst1.compose(fst2)
+        queue = queue[2:]+[fst1]
+        queue.sort(key=lambda fst: fst.number_of_states())
+    
+    return queue[0]
+
+
+def string2string(fst, rlist):
+    eregex = [ regex for regex in rlist if regex.startswith('"<E>"') ]
+    nregex = [ regex for regex in rlist if regex not in eregex ]
+
+    stderr.write('Composing...\n')
+    # Apply regular substution and deletion rules
+    if nregex != []:
+        fst.compose(serial_compile(nregex))
+    # Add epsilon literals by expanding separators
+    expand(fst)
+    # Apply epsilon rules
+    if eregex != []:
+        fst.compose(serial_compile(eregex))
+
+
+def separators(fst):
+    regex = '0 -> "<S>"'
+    fst.compose(hfst.regex(regex))
+
+
+def double(fst, chars):
+    regex = '0 -> "<D>" || "<S>" _ [ ? - "<S>" ]'
+    fst.compose(hfst.regex(regex))
+    r = []
+    for char in chars:
+        #r.append('"<D>" '+char+' -> '+char+' "<.>" '+char)
+        r.append('"<D>" '+char+' -> '+char+' '+char)
+    regex = ' ,, '.join(r)
+    fst.compose(hfst.regex(regex))
+
+
+def single(fst, chars):
+    #regex = '[ "<S>" [ ? - "<.>" ]+ ] -> 0'
+    regex = '"<S>" ? -> 0' 
+    fst.compose(hfst.regex(regex))
+
+
+def delete_aux(fst):
+    regex = '[ "<P>" | "<S>" | "<E>" | "<D>" | "<.>" ] -> 0'
+    fst.compose(hfst.regex(regex))
+
+
+def compile(filename, outfile, feats_file):
+    stderr.write('Compiling into FST...\n')
+    rlist = open(filename, 'r', encoding='utf8').read().rstrip(' ;').split(',,\n')
+    # Get all known input-level symbols:
+    chars = get_chars(feats_file)
+    # Create transducer
+    # Add padding to start and end of string
+    fst = hfst.regex('0 -> "<P>" || .#. _ ,, 0 -> "<P>" || _ .#.')
+    # Add char separators
+    separators(fst)
+    # Double known symbols and add dummies
+    double(fst, chars)
+    # Perform string2string translation
+    string2string(fst, rlist)
+    # Delete preceding input-level symbol
+    single(fst, chars)
+    # Delete auxiliary symbols
+    delete_aux(fst)
+    # Minimize and write into .hfst file
+    fst.minimize()
+    fst.convert(hfst.ImplementationType.HFST_OLW_TYPE)
+    ostr = hfst.HfstOutputStream(filename=outfile, type=hfst.ImplementationType.HFST_OLW_TYPE)
+    ostr.write(fst)
+    ostr.flush()
+    ostr.close()
+    stderr.write('Done.\n')
+
+
+if __name__ == "__main__":
+    argv.append(argv[1].replace('_regex.txt', '.hfst'))
+    compile(argv[1], argv[2], argv[3])
---
+++ train/train-models.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+# This script trains a series of error models (unstructured classifiers)
+# that can be used in simple string-to-string translation tasks.
+# Intermediate files (string pairs, features, rule sets) are also created. 
+#
+# - Training data should be plaintext file of tab-separated pairs of strings
+# - Feature extraction works as follows:
+#     1) String pairs are aligned on character level.
+#     2) Features (subsitutions + contexts) used by the unstructured classifier
+#        are extracted. Feature specifications are hard-coded but can be modified.
+# - Features whose frequency exceed $T are rewritten as weighted parallel-replace rules.
+# - Replace rules are compiled into FSTs i.e. error models.
+#
+# Resulting error models can be used with e.g. hfst-ospell
+
+BASE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+FNAME=$1
+
+# Minimum and maximum thresholds
+T_MIN=3
+T_MAX=9
+
+# Feature extraction
+PFX=$( echo $FNAME | sed -r 's/[.][a-z][a-z]?[a-z]?$//g' )
+PAIRS_FILE=$PFX\_pairs.txt
+FEATS_FILE=$PFX\_feats.txt
+$BASE/data2pairs.py $FNAME > $PAIRS_FILE
+$BASE/pairs2features.py $PAIRS_FILE > $FEATS_FILE
+
+# Rule formulation and compilation
+for T in $( seq $T_MIN $T_MAX ) ; do
+    REGEX_FILE=$PFX\_$T\_regex.txt
+    OLFST_FILE=$PFX\_$T.hfst
+    $BASE/features2rules.py $FEATS_FILE $T > $REGEX_FILE
+    $BASE/compile-rules.py $REGEX_FILE $OLFST_FILE $FEATS_FILE
+done
